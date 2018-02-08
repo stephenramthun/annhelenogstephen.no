7bde5d567968f9e7a4432b059534b6dee1e15b50
stephenramthun <stephenramthun@gmail.com>
2018-02-08 16:04:33 +0100

    Redo text, add section and link to map

--- app/translator.py
+++ app/translator.py
@@ -8,15 +8,15 @@ class Translator():
                 'header1': 'Ann Helen and Stephen',
                 'header2': 'have the pleasure of inviting',
                 'header3': 'to their wedding, Saturday August 11th. 2018',
-                'header4': 'in Dale Church in Luster, Sogn og Fjordane, 1:00 pm.',
+                'header4': 'in Dale Church in Luster, Sogn og Fjordane, 2:00 pm.',
                 'header5': 'The reception will be held at Tørvis Hotel from 4:30 pm. We hope you will be able to celebrate our day with us!',
 
                 # Program
                 'program_header': 'PROGRAM',
-                'program_body_1': '1:00 pm - Wedding Ceremony in Dale Church in Luster.',
-                'program_body_2': '4:30 pm - Welcome drink at Tørvis Hotel, Marifjøra. Photography session.',
+                'program_body_1': '2:00 pm - Wedding Ceremony in Dale Church in Luster.',
+                'program_body_2': '4:30 pm - Welcome to Tørvis Hotel, Marifjøra. Photography session.',
                 'program_body_3': '5:30 pm - Dinner.',
-                'program_body_4': '9:00 pm - Party, cake and fun.',
+                'program_body_4': '9:30 pm - Party, cake and fun.',
                 'program_body_5': '12:30 pm - Late night snack.',
 
                 # Wedding
@@ -27,6 +27,14 @@ class Translator():
                 'reception_header': 'RECEPTION',
                 'reception_body_1': 'Tørvis Hotel is in Marifjøra, about 20 minutes from downtown Sogndal on the way to the church. The hotel itself has its roots from the middle of the 17th century.',
                 'reception_body_2': 'William is the toastmaster for the evening. If you would like to give a speech during the reception, send him an email at wmorris@live.no. If you would rather have him relay a message from you to the newlyweds, tweet @bestmansays',
+
+                # Barbeque
+                'barbeque_header': 'FRIDAY AUGUST 10',
+                'barbeque1': 'The wedding couple with family invite all guests to an American barbeque party. We hope that family and friends will come and get to know each other before the big day.',
+                'barbeque2': 'Where: The Brendehaug family cabin, Selseng.',
+                'barbeque3': 'When: 6:00 pm',
+                'barbeque4': 'Dress code for the evening is nice and practical clothing. Remember to bring warm clothes in case the temperature drops during the evening, as the party is mainly held outdoors.',
+                'barbeque5': 'Map',
 
                 # Accomodation
                 'accomodation_header': 'ACCOMODATION',
@@ -72,25 +80,34 @@ class Translator():
                 'header1': 'Ann Helen og Stephen',
                 'header2': 'har gleden av å invitere',
                 'header3': 'til bryllaup, laurdag 11. august 2018',
-                'header4': 'i Dale kyrkje i Luster kl 13:00.',
+                'header4': 'i Dale kyrkje i Luster kl 14:00.',
                 'header5': 'Middag og fest vil finne stad på Tørvis hotell frå kl. 16:30. Vi håpar de har moglegheit til å feire dagen med oss!',
+                'header6': 'Kleskode for dagen er bunad, pen kjole eller dress.',
 
                 # Program
                 'program_header': 'PROGRAM FOR DAGEN',
-                'program_body_1': '13:00 - Vielse i Dale kyrkje i Luster',
-                'program_body_2': '16:30 - Velkomstdrink på Tørvis hotell. Fotografering.',
+                'program_body_1': '14:00 - Vielse i Dale kyrkje i Luster',
+                'program_body_2': '16:30 - Velkommen til Tørvis hotell. Fotografering.',
                 'program_body_3': '17:30 - Middag.',
-                'program_body_4': '21:00 - Fest, kake og moro.',
+                'program_body_4': '21:30 - Fest, kake og moro.',
                 'program_body_5': '00:30 - Nattmat.',
 
                 # Wedding
                 'wedding_header': 'VIELSEN',
-                'wedding_body': 'Dale kirke ligger i Luster kommune i Sogn og Fjordane, ca 40 minutter unna Sogndal sentrum. Steinkirken er fra første halvdel av 1200-tallet, med interiør fra forskjellige kunstepoker, bl.a. renessansen og barokken.',
+                'wedding_body': 'Dale kyrkje ligg i Luster kommune i Sogn og Fjordane, ca 20 minutt med bil frå Tørvis hotell. Steinkyrkja er frå første halvdel av 1200-talet, med interiør frå ulike kunstperiodar, med anna renessansen og barokken. Prest Geir Paulsen vil vie brudeparet',
 
                 # Reception
                 'reception_header': 'MIDDAG OG FEST',
-                'reception_body_1': 'Tørvis hotell ligger i Marifjøra, ca. 20 minutter med bil fra Sogndal på veien til Dale kirke. Hotellet er fra rundt midten av 1600-tallet.',
-                'reception_body_2': 'William vert kveldens toastmaster. Om du ynskjer å halde tale kan du ta kontakt med han på wmorris@live.no eller telefon 905 22 339. På twitterkontoen @bestmansays kan du leggje igjen ei kort helsing til brureparet om du helst vil sleppe taletid, men likevel har noko på hjartet.',
+                'reception_body_1': 'Tørvis hotell ligg i Marifjøra, ca. 20 minutt med bil frå Sogndal. Hotellet vart bygd på midten av 1600-talet.',
+                'reception_body_2': 'William vert toastmaster for kvelden. Om du ynskjer å halde tale kan du ta kontakt med han på wmorris@live.no eller telefon 905 22 339. På twitterkontoen @bestmansays kan du leggje igjen ei kort helsing til brureparet om du helst vil sleppe taletid, men likevel har noko på hjartet.',
+
+                # Barbeque
+                'barbeque_header': 'FREDAG 10. AUGUST',
+                'barbeque1': 'Brudeparet med familiar inviterer alle gjester til amerikansk grillfest. Vi håpar familie og vener vil kome for å helse på oss og kvarandre og verte kjende før den store dagen.',
+                'barbeque2': 'Kvar: Familien Brendehaug si familiehytte, Selseng.',
+                'barbeque3': 'Tidspunkt: 18:00',
+                'barbeque4': 'Kleskode for kvelden er pene og praktiske klede. Husk å ta med varme klede i fall det vert kjølig utover kvelden (festen vert halden ute).',
+                'barbeque5': 'Kart',
 
                 # Accomodation
                 'accomodation_header': 'OVERNATTING',
@@ -101,33 +118,33 @@ class Translator():
                 'gifts_body': 'Vi ynskjer fyrst og fremst å samle familie og vener på dagen vår og tykkjer ikkje gåver er viktig. Om de likevel vil gje oss noko, likar vi gjenbruk og har de ein ting av retro årgang, som de tenkjer vil få eit nytt liv hjå oss, er det storarta! Elles finn de ei ynskjeliste på Kitchn sine nettsider under fana "Bryllup - finn liste". Vi ynskjer oss òg pengar til bryllaupsreise.',
 
                 # RSVP
-                'rsvp_1': 'Vi vil gjerne komme.',
-                'rsvp_2': 'Vi kan dessverre ikke komme.',
-                'reg': 'Dine registrerte deltakere:',
-                'reg_answer_1': 'Takk for svaret! Vi gleder oss til å se deg!',
-                'reg_answer_2': 'Legg til navn og detaljer om deltakerne som kommer fra dere nedenfor.',
-                'reg_answer_3': 'Takk for svaret. Håper vi sees snart.',
-                'reg_answer_4': 'Vennligst oppgi en epostadresse slik at vi kan nå dere:',
-                'reg_answer_5': 'Din registrerte epostadresse:',
-                'reg_answer_6': 'Endre din registrerte epost:',
+                'rsvp_1': 'Vi vil gjerne kome.',
+                'rsvp_2': 'Vi kan diverre ikkje kome.',
+                'reg': 'Dine registrerte deltakarar:',
+                'reg_answer_1': 'Takk for svaret! Vi gledar oss til å sjå dykk!',
+                'reg_answer_2': 'Legg til namn og detaljar om kvar enkelt gjest du melder på i skjemaet under.',
+                'reg_answer_3': 'Takk for svaret. Håpar vi sjåast snart.',
+                'reg_answer_4': 'Ver venleg å oppgi en epostadresse slik at vi kan nå dykk:',
+                'reg_answer_5': 'Di registrerte epostadresse:',
+                'reg_answer_6': 'Endre di registrerte epostadresse:',
 
                 # Register
-                'reg_1': 'Fornavn',
-                'reg_2': 'Etternavn',
-                'reg_3': 'Allergier, om aktuelt',
+                'reg_1': 'Fornamn',
+                'reg_2': 'Etternamn',
+                'reg_3': 'Allergiar, om aktuelt',
                 'reg_4': 'Vegan',
                 'reg_5': 'Vegetar',
                 'reg_6': 'Barnemeny',
                 'reg_7': 'Kryss av om',
-                'reg_8': 'Legg til deltaker',
+                'reg_8': 'Legg til deltakar',
                 'reg_9': 'Legg til',
 
                 # Contact
                 'contact_0': 'KONTAKT',
                 'contact_1': 'Brud',
                 'contact_2': 'Brudgom',
-                'contact_3': 'Brudens forlover',
-                'contact_4': 'Brudgoms forlover',
+                'contact_3': 'Bruda sin trulovar',
+                'contact_4': 'Brudgomen sin trulovar',
                 'contact_5': 'E-post'
             }
         }
